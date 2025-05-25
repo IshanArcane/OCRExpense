f85f7019766d5414c3da09986546a8422f4b9cad
Ishan M <imamidpalliwar@gmail.com>
2025-05-25 12:54:46 +0530

    Add files via upload

---
+++ PythonPRO/tempCodeRunnerFile.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+from PIL import Image
+import pytesseract
+import io
+
+# Set the path to the tesseract executable
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+app = Flask(__name__)
+
+
+@app.route('/ocr', methods=['POST'])
+def perform_ocr():
+    try:
+        if 'image' not in request.files:
+            return jsonify({'error': 'No image provided'}), 400
+        
+        image_file = request.files['image']
+        image = Image.open(io.BytesIO(image_file.read()))
+        extracted_text = pytesseract.image_to_string(image)
+        
+        return jsonify({'text': extracted_text}), 200
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
